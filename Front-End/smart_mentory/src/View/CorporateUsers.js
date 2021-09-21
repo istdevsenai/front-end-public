be9c17f115299de7538dceb8ed6fa5a5858127ef
NicolasRSJ <84856034+NicolasRSJ@users.noreply.github.com>
2021-09-21 13:26:02 -0300

    init project

---
+++ Front-End/smart_mentory/src/View/CorporateUsers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CorporateUsers extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CorporateUsers;
